a4ca4c074bce73946a9cc1a7e5516208c1735095
ShunHidaka <stern@DESKTOP-TV5D8M4>
2024-12-17 16:02:06 +0900

    JP comments to EN comments

--- src-matlab/sample_sminres.m
+++ src-matlab/sample_sminres.m
@@ -1,32 +1,30 @@
 %
 % sample program for shifted Krylov solver
 % First update : 2024/12/12
-% Last update  : 2024/12/13
+% Last update  : 2024/12/17
 % Created by "ShunHidaka (https://github.com/ShunHidaka)"
 %
 
-% 行列Aの用意
+% Prepare matrix $A, b, sigma$
 % https://math.nist.gov/MatrixMarket/mmio/matlab/mmiomatlab.html
 % http://www.damp.tottori-u.ac.jp/~hoshi/elses_matrix/ELSES_MATRIX_VCNT4000std_20130517.tgz
 [A, rows, cols, entries] = mmread("ELSES_MATRIX_VCNT4000std_A.mtx");
 N = rows;
-% シフトsigmaの用意
+% Prepare shits $sigma^{(m)}$
 M = 3;
 sigma = zeros(M, 1);
 for m = 1:1:M
-    % VCNT4000std_Aは正定値でないので最小固有値をシフトさせて正定値にする
-    sigma(m) = 1.0 + 0.001*m;
+    sigma(m) = 0.001*m;
 end
-% 右辺ベクトルbの用意
 b = ones(N, 1);
-% 収束関連の定数を設定
+
 max_itr = 100000;
 threshold = 1e-13;
 
-% 解かせる
+% Solve by shifted MINRES method
 [x, flag, rres, itrs] = shifted_MINRES(A, b, N, sigma, M, max_itr, threshold);
 
-% 実行結果の検証
+% verification of results
 true_res = zeros(M,1);
 for m = 1:1:M
     r = b - (A*x(:,m) + sigma(m)*x(:,m));
--- src-matlab/shifted_minres.m
+++ src-matlab/shifted_minres.m
@@ -1,54 +1,57 @@
 %
 % shifted MINRES method by MATLAB
 % First update : 2024/12/12
-% Last update  : 2024/12/13
+% Last update  : 2024/12/17
 % Created by "ShunHidaka (https://github.com/ShunHidaka)"
 % 実数向けに作成、複素数用ではない
-% 引数：
-%   行列A, 右辺ベクトルrhs, 行列サイズN,
-% 　シフトsigma, シフト数M,
-%   最大反復回数max_itr, 閾値threshold
-% 返り値：
-%   近似解x(N行M列)
-%   フラグflag
-%   相対残差rres（M行）
-%   収束したときの反復回数itrs（M行）
+% Solve shifted linear systems
+% (A + \sigma^{(m)}I_N)\vb{x}^{(m)} = \vb{b},  (m=1,\dots, M).
+% argument：
+%   "Matrix" A, "Right-Hand-Side-vector" rhs, "Matrix-size" N,
+% 　"Shifts" sigma, "Number-of-shits" M,
+%   "Maximum-iteration" max_itr, "relative-residual-norm-threshold" threshold
+% Return：
+%   "Approximate-solution" x (M \times N)
+%   "Status" flag
+%   "Relative-residual-norm" rres (M)
+%   "Converged iterations" itrs (M)
 %
 
 function [x, flag, rres, itrs] = shifted_MINRES(A, rhs, N, sigma, M, max_itr, threshold)
-    x    = zeros(N, M);  % 近似解
-    flag = 1;           % 正常に収束したかどうかを示すフラグ、0なら正常に実行
-    rres = zeros(M, 1); % 各シフト方程式の相対残差
-    itrs = zeros(M, 1); % 収束したときの反復回数
-    % 使用する変数の宣言
-    q       = zeros(N, 3);  % Lanczos基底 q_{j-1}, q_{j}, q_{j+1}
-    alpha   = zeros(1, 1);  % Lanczos過程で得られる定数
-    beta    = zeros(2, 1);  % Lanczos過程で得られる定数 beta_{j-1}, beta_{j}
-    r       = zeros(3, M);  % 三重対角行列用のベクトル
-    G1      = zeros(2,2,M); % Givens回転行列を格納する配列, G_{j-2}
-    G2      = zeros(2,2,M); % Givens回転行列を格納する配列, G_{j-1}
-    G3      = zeros(2,2,M); % Givens回転行列を格納する配列, G_{j}
-    p       = zeros(N,3,M); % 補助ベクトル, p(:,1,m)=p_{j-2}^{(m)}, p(:,2,m)=p_{j-1}^{(m)}, p(:,3,m)=p_{j}^{(m)}
-    f       = zeros(M);     % 解の更新に用いる一時変数
-    h       = zeros(M);     % 残差のノルム
-    is_conv = zeros(M, 1);  % 収束した方程式を管理するベクトル
-    % 変数の初期化
+    x    = zeros(N, M); % Approximate solutions
+    flag = 1;           % if flag=0 then all Eqs converged
+    rres = zeros(M, 1); % relative residual norm of each Eqs
+    itrs = zeros(M, 1); % converged iterations
+    % Declaer variables
+    q       = zeros(N, 3);  % Lanczos basis, q_{j-1}, q_{j}, q_{j+1}
+    alpha   = zeros(1, 1);  % Lanczos constant
+    beta    = zeros(2, 1);  % Lanczos constant, beta_{j-1}, beta_{j}
+    r       = zeros(3, M);  % Tridiagonal matrix
+    G1      = zeros(2,2,M); % Givens rotation matrix, G_{j-2}
+    G2      = zeros(2,2,M); % Givens rotation matrix, G_{j-1}
+    G3      = zeros(2,2,M); % Givens rotation matrix, G_{j}
+    p       = zeros(N,3,M); % auxiliary vec, p(:,1,m)=p_{j-2}^{(m)}, p(:,2,m)=p_{j-1}^{(m)}, p(:,3,m)=p_{j}^{(m)}
+    f       = zeros(M);     % temporary variable for updating approximates
+    h       = zeros(M);     % residual norm on Algorithm
+    is_conv = zeros(M, 1);  % status of each Eqs
+    % Initialize variables
     r0nrm = norm(rhs);
     q(:,2) = rhs / r0nrm;
     for m = 1:1:M
+        itrs(m) = N;
         f(m) = 1;
         h(m) = r0nrm;
         is_conv(m) = 0;
     end
-    % メインループ
+    % Main loop
     conv_num = 0;
     for j = 1:1:max_itr
-        % Lanczos過程
+        % Lanczos process
         q(:,3) = A*q(:,2) - beta(1)*q(:,1);
         alpha(1) = dot(q(:,3), q(:,2));
         q(:,3) = q(:,3) - alpha(1)*q(:,2);
         beta(2) = norm(q(:,3));
-        % 各シフト方程式の更新
+        % update each shift Eq
         for m = 1:1:M
             if is_conv(m) == 1
                 continue;
@@ -70,7 +73,7 @@ function [x, flag, rres, itrs] = shifted_MINRES(A, rhs, N, sigma, M, max_itr, th
             h(m) = abs(G3(2,1,m))*h(m);
             G1(:,:,m) = G2(:,:,m);
             G2(:,:,m) = G3(:,:,m);
-            % シフト方程式の収束判定
+            % Determine convergence for each shift Eq
             rres(m) = h(m)/r0nrm;
             if rres(m) <= threshold
                 is_conv(m) = 1;
@@ -82,7 +85,7 @@ function [x, flag, rres, itrs] = shifted_MINRES(A, rhs, N, sigma, M, max_itr, th
         q(:,1) = q(:,2);
         q(:,2) = q(:,3);
         beta(1) = beta(2);
-        % 全ての方程式が収束したかの判定
+        % Determine convergenc for all Eqs
         if conv_num == M
             flag = 0;
             break;
